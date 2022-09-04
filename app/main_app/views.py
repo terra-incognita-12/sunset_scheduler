b4a2430804b4db3efa37ce4f76ed68dec08a9c1c
antonchuss <anton.gorshenin27@gmail.com>
2022-09-04 15:38:57 -0700

    Login page skeleton, organize templates

--- app/main_app/views.py
+++ app/main_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'index.html', {})
+	return render(request, 'index/index.html', {})
+
+def login(request):
+	return render(request, 'auth/login.html', {})
